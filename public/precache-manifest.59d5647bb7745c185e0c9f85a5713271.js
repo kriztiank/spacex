30cc28ab10ec082066aa976d83d88a5babf63670
kriztiank <kriztiank@gmail.com>
2020-09-07 11:52:42 +0200

    init

---
+++ public/precache-manifest.59d5647bb7745c185e0c9f85a5713271.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65a84acd61ee92287f1f8d8ddb8780a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "339c70e5a6e8862a5202",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "3c64c75af10263ca29bf",
+    "url": "/static/js/2.80d397bc.chunk.js"
+  },
+  {
+    "revision": "1168987de8c022b65d0b713d94cc1242",
+    "url": "/static/js/2.80d397bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "339c70e5a6e8862a5202",
+    "url": "/static/js/main.b3ee89d2.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  }
+]);
